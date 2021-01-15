e2b96e268158fb6dfa873cb937b03f2d90d61efa
shunf4 <shun1048576@gmail.com>
2021-01-15 21:14:27 +0800

    fix: build error of linux because of redundant import

--- dns/get_dhcp_nameserver_linux.go
+++ dns/get_dhcp_nameserver_linux.go
@@ -2,10 +2,6 @@
 
 package dns
 
-import (
-	"golang.org/x/sys/windows/registry"
-)
-
 func GetCurrDhcpNameservers() (nameservers []string, domain string, searchs []string) {
 	// Not implemented
 	return []string{}, "", []string{}
